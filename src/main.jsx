4621dca86cefd2a3b8b136f13bbe09624a36c950
Niteesh Singh <niteeshs@adobe.com>
2024-12-24 02:35:14 +0530

    added 404.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/reset.css'; // Reset CSS\
 import './styles/index.css'; // Main CSS
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap styles
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>,
+  document.getElementById('root')
+);
