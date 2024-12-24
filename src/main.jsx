f6d88f3db4f55211ee0303e9a4356f5f618aa1b1
Niteesh Singh <niteeshs@adobe.com>
2024-12-24 12:56:10 +0530

    added 404.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './styles/reset.css'; // Reset CSS\
 import './styles/index.css'; // Main CSS
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap styles
